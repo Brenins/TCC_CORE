ad925bf2f657f80b47c0e84e6d011abdc978656e
Breno Ramos <breninsramos@gmail.com>
2022-08-02 21:21:31 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/551191981e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>REPREST - HOME</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">
+            <img src="images/logoteste.png" alt="">
+        </div>
+        <div class="menu">
+            <div class="dropdown">
+                <button class="dropbtn rounded btn btn-primary">Cadastros</button>
+                <div class="dropdown-conteudo rounded">
+                    <a href="#">Cadastro de Moradores</a>
+                    <a href="#">Cadastro de Itens</a>
+                    <a href="#">Cadastro de Usuarios</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn rounded btn btn-primary">Itens e Locais</button>
+                <div class="dropdown-conteudo rounded">
+                    <a href="#">Lista de Itens</a>
+                    <a href="#">Lista de Moradores</a>
+                    <a href="#">Locais de Uso Comum</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn rounded btn btn-primary">Reservas e Emprestimos</button>
+                <div class="dropdown-conteudo rounded">
+                    <a href="#">Controle de Reservas</a>
+                    <a href="#">Controle de Emprestimos</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn rounded btn btn-primary">Relatorios</button>
+                <div class="dropdown-conteudo rounded">
+                    <a href="#">Itens em Emprestimo</a>
+                    <a href="#">Reservas em Andamento</a>
+                    <a href="#">Lista de Moradores</a>
+                </div>
+            </div>
+        </div>
+        <div class="opcoes">
+        <div class="dropdown">
+                <button class="dropbtn rounded btn btn-success">Perfil <i class="bi bi-person-fill"></i></button>
+                <div class="dropdown-conteudo rounded ">
+                    <a href="#">Alterar Senha</a>
+                    <a href="#">Sair do Sistema</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
